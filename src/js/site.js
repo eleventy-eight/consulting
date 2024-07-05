536775f682ab1ace4e314a8242fad62ff76bd236
Paul George <admin@eleventy8.com>
2024-07-05 16:03:40 +0100

    More structure and styles

--- src/js/site.js
+++ src/js/site.js
@@ -53,3 +53,28 @@ axios
     .catch(function (response) {
         console.error(response);
     });
+
+// Back to top button
+let backToTopBtn = document.getElementById("backToTopBtn");
+
+// When the user scrolls down 100px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (
+        document.body.scrollTop > 100 ||
+        document.documentElement.scrollTop > 100
+    ) {
+        backToTopBtn.style.display = "block";
+    } else {
+        backToTopBtn.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
